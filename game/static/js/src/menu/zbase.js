6b381887b35f0e4ff4c07341d44ddb20fa653595
LiuFushuang <liufushuang@sealand-navigator.cn>
2026-03-10 17:21:28 +0800

    初步实现联机

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -25,22 +25,24 @@ class AcGameMenu{
         this.start();
     }
 
-start(){
-    this.add_listening_events();
-}
+    start(){
+        this.add_listening_events();
+    }
 
     add_listening_events(){
         let outer = this;
         this.$single_mode.click(function(){
             outer.hide();
-            outer.root.playground.show();
+            outer.root.playground.show("single mode");
         });
         this.$multi_mode.click(function(){
-            console.log("click multi mode");
+            outer.hide();
+            outer.root.playground.show("multi mode")
         });
         this.$settings.click(function(){
             console.log("click settings");
             outer.root.settings.logout_on_remote();
+
         });
     }
 
